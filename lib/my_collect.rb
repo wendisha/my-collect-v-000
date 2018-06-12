3f72bddab4d3817a5ee2864f2545fcdf0d51a501
Wendy Calderon <wendycalderon@gmail.com>
2018-06-12 01:13:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect (collection)
+  counter = 0
+  new_collection = []
+  while counter < collection.length
+    new_collection << yield(collection [counter])
+    counter += 1
+  end
+  new_collection
+end
